63895d77e33d0d9e3224f623db56df81ee013129
AfhamFardeen <developer1@3plusgames.com>
2023-08-22 07:38:29 +0530

    aasdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { data, setData } = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/getData")
+            .then((res) => res.json())
+            .then((json) => {
+                setData({ json });
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, [])
+
+    return (
+        <div className="body">
+            <div className="container">
+                {data === undefined ? <p>loading data...</p> :
+                    data.map((elem) => {
+                        return (elem.type === 'Recever' ?
+                            <div className="message-blue">
+                                <p className="message-content">{elem.msg}</p>
+                                <div className="message-timestamp-left">{elem.time}</div>
+                            </div> :
+                            <div className="message-orange">
+                                <p className="message-content">{elem.msg}</p>
+                                <div className="message-timestamp-right">{elem.time}</div>
+                            </div>
+                        )
+                    })}
+            </div>
+            <div className="sendButtons">
+                <button className="sendButton">Send</button>
+                <button className="createRoom">Create Room</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
